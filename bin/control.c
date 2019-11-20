df7d300c75ffc89ce670f5d8605984ac082ed5e4
Robert Smith <robertets88@gmail.com>
2019-11-19 21:23:45 -0500

    created libray, header file and updated control to link to that header file

--- bin/control.c
+++ bin/control.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include </Users/robertsmith/Desktop/depanna/lib/nmap/NmapControl.h>
 
 int main()
 {
-	// Message the user
-	printf("Hello. Your audit is about to start...\n");
-
-
-	/*  system accesses the command line */
-	int trial = system("~/Desktop/vir_prog/is_alive_ping.sh");
-	printf("%d\n", trial);
-
+	
+	
+	
+	printf("Hello world");
 
 
 	return 0;
