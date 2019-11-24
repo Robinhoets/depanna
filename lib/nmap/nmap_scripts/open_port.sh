7ed92537dcc58786bdc3dd26090b44fcdf0f9e4f
Robert Smith <robertets88@gmail.com>
2019-11-23 22:02:39 -0500

    open ports script now detects all ips on network

--- lib/nmap/nmap_scripts/open_port.sh
+++ lib/nmap/nmap_scripts/open_port.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+# find network and save as variable
+ipadd="$(ifconfig | grep "inet " |  grep -v 127.0.0.1 | cut -d\  -f2)"
+iprange="${ipadd}/24"
+echo $iprange
+# scan that network for ip address connected. Save each ip as a variable.
+nmap -n -sn $iprange -oG - | awk '/Up$/{print $2}'
+
+
+
 # create a variable that holds this computer's ipaddress
-v=$(curl ifconfig.me)
+#v=$(curl ifconfig.me)
 
 # run nmap without ping so that every port is scanned regardless of whether it
 # returns a ping or not.
 # save the results in t.txt in current directory
-nmap -Pn $v>>t.txt
+#nmap -Pn $v>>t.txt
