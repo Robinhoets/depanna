a10de3abca246db0599d6a4bc7689b64f683d516
Robert Smith <robertets88@gmail.com>
2019-11-23 22:38:14 -0500

    edited script to save connected devices to text file and save open ports to text file

--- lib/nmap/nmap_scripts/open_port.sh
+++ lib/nmap/nmap_scripts/open_port.sh
@@ -5,8 +5,7 @@ ipadd="$(ifconfig | grep "inet " |  grep -v 127.0.0.1 | cut -d\  -f2)"
 iprange="${ipadd}/24"
 echo $iprange
 # scan that network for ip address connected. Save each ip as a variable.
-nmap -n -sn $iprange -oG - | awk '/Up$/{print $2}'
-
+nmap -n -sn $iprange -oG - | awk '/Up$/{print $2}' >> test.txt
 
 
 # create a variable that holds this computer's ipaddress
@@ -15,4 +14,4 @@ nmap -n -sn $iprange -oG - | awk '/Up$/{print $2}'
 # run nmap without ping so that every port is scanned regardless of whether it
 # returns a ping or not.
 # save the results in t.txt in current directory
-#nmap -Pn $v>>t.txt
+nmap -iL test.txt >> final.txt
