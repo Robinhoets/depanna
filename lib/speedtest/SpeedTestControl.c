bf1329e54a5027af8ff31f1a5ce6533d63d63c07
Robert Smith <robertets88@gmail.com>
2019-11-30 18:30:43 -0500

    commented the SpeedTestControl.c file

--- lib/speedtest/SpeedTestControl.c
+++ lib/speedtest/SpeedTestControl.c
@@ -5,7 +5,9 @@
 void getSpeedTestReport()
 {
         /*
-	
+		Accesses the command line to run SpeedTestScript.
+		@param takes location in directory of script
+		@return returns bash script to command line.
         */
         system("/Users/robertsmith/Desktop/depanna/lib/speedtest/speedtest_scripts/speed_test_curl.sh");
 }
